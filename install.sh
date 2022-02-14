415e24e09a6bef565102b76a7898b9ce0959b0cd
枫雨 <737345039@qq.com>
2022-02-15 05:59:13 +0800

    test 3.0

--- install.sh
+++ install.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-WORKPATH=/DDTVLiveRec
-BACKUPSPATH=/backups
-RMFILENAME=ReleaseInstalled
-DDTVLiveRecVERSION=` wget -qO - https://api.github.com/repos/CHKZL/DDTV2/releases/latest | awk '/tag_name/{print $5;exit}' FS='[r"]' `
+Tmp_File=DDTV_Tmp
+Backups_Path=/DDTV_Backups
+Download_Url=$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/\/DDTV_W/{print $4;exit}' FS='"')
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' >/etc/timezone
 
-echo "**** install DDTVLiveRec ****"
-    cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' >/etc/timezone
-    wget https://github.com/CHKZL/DDTV2/releases/latest/download/DDTVLiveRec-${DDTVLiveRecVERSION}.zip -O $RMFILENAME.zip
-    unzip $RMFILENAME.zip -d $RMFILENAME
-    mv $RMFILENAME/*/*/* $WORKPATH
-
-echo "**** prepare for start.sh ****"
-    mkdir $BACKUPSPATH
-    mv $WORKPATH/DDTVLiveRec.dll.config $BACKUPSPATH/DDTVLiveRec.dll.config
-    mv $RMFILENAME/*/*RoomListConfig.json $BACKUPSPATH/RoomListConfig.json
-    echo -e "[User]\n\nCookie=\ncsrf=" > $BACKUPSPATH/BiliUser.ini
-    sed -i '1c [User]' $BACKUPSPATH/BiliUser.ini
+echo "**** install DDTV ****"
+    wget $Download_Url -O $Tmp_File.zip
+    unzip $Tmp_File.zip -d $Tmp_File
+    File_Path=$(unzip -l $Tmp_File.zip | awk "/dll/{print \$4;exit}" FS=' ')
+    File_Path=${File_Path%/*}
+    mkdir $Backups_Path
+    mv -f $Tmp_File/$File_Path/* $Backups_Path
 
 echo "**** clean up ****"
-    rm -rf $RMFILENAME*
+    rm -rf $Tmp_File*
