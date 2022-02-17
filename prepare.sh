8cadb5b534c383b7de351dc3b36a491b81c6d08c
枫雨 <737345039@qq.com>
2022-02-17 18:57:48 +0800

    Update to DDTV 3.0 && remove test files from master

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+wget "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')"
+File_Path=$(unzip -l *.zip | awk "/dll/{print \$4;exit}" FS=' ')
+unzip *.zip
+mkdir root/DDTV root/DDTV_Backups
+mv -f ${File_Path%/*}/* root/DDTV_Backups
+cd root/DDTV_Backups && dotnet DDTV_Update.dll docker
