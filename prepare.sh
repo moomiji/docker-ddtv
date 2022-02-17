dbf1d58b32a95fad9d207a474896a688824a3b20
枫雨 <737345039@qq.com>
2022-02-17 19:07:44 +0800

    fix && prepare for merge

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e; set -u
 wget "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')"
 File_Path=$(unzip -l *.zip | awk "/dll/{print \$4;exit}" FS=' ')
 unzip *.zip
