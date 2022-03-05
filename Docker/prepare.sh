486be4f43627293e4ff2bcfbb2e34c0f6038d211
枫雨 <737345039@qq.com>
2022-03-06 05:47:57 +0800

    Build nginx test
    Update DDTV_WEB_Server_Docker.yml
    
    Build nginx test

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e; set -u
-wget "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')"
+wget "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')" && 
+wget $1 # 要删
 File_Path=$(unzip -l *.zip | awk "/dll/{print \$4;exit}" FS=' ')
 unzip *.zip
-mkdir root/DDTV root/DDTV_Backups
+mkdir -p root/DDTV root/DDTV_Backups
 mv -f ${File_Path%/*}/* root/DDTV_Backups
 cd root/DDTV_Backups && dotnet DDTV_Update.dll docker
+
+
+cd ..
+cd ..
+mkdir -p /usr/share/nginx/html
+mv -f dist/* /usr/share/nginx/html/*
