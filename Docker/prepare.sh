982b6e13b8a8c555a4c8796b0d22757e8c8b6b76
枫雨 <737345039@qq.com>
2022-03-06 05:57:19 +0800

    Update prepare.sh

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e; set -u
 wget "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')" && 
-wget $1 # 要删
 File_Path=$(unzip -l *.zip | awk "/dll/{print \$4;exit}" FS=' ')
 unzip *.zip
 mkdir -p root/DDTV root/DDTV_Backups
@@ -11,5 +10,8 @@ cd root/DDTV_Backups && dotnet DDTV_Update.dll docker
 
 cd ..
 cd ..
+rm -rf *.zip
+wget -q $1
+unzip *.zip
 mkdir -p /usr/share/nginx/html
 mv -f dist/* /usr/share/nginx/html/*
