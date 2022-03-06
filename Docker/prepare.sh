acb37622fe3afddf468353cb4129e72ff9a717f4
枫雨 <737345039@qq.com>
2022-03-06 21:26:13 +0800

    chore: Update
    Update README.md
    
    Update .sh file
    
    Update default.conf.template
    
    Update prepare.sh
    
    Update DDTV_WEB_Server_Docker.yml

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e; set -u
 wget -q "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')"
-Server_File_Path=$(unzip -l ./*.zip | awk "/files:.*\.zip/{print \$2;exit}" FS='/')
+Server_File_Path=$(unzip -l ./*.zip | awk "/dll/{print \$4;exit}" FS=' ')
 
-#wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk '/[Ss]erver/{print $4;exit}' FS='"')"
+#wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk "/files:.*\.zip/{print \$2;exit}" FS='/')"
 wget "$1"
 unzip "*.zip"
 
-mkdir -p root/DDTV root/DDTV_Backups/static nginx/usr/share/nginx/html
-#cp -vur dist/* root/DDTV_Backups/static
-mv -vu dist/* nginx/usr/share/nginx/html
-mv -vu "${Server_File_Path%/*}"/* root/DDTV_Backups
+mkdir -p root/DDTV \
+         root/DDTV_Backups \
+         nginx/DDTV_Backups
+mv -v dist/* nginx/DDTV_Backups
+mv -v "${Server_File_Path%/*}"/* root/DDTV_Backups
 cd root/DDTV_Backups && dotnet DDTV_Update.dll docker
