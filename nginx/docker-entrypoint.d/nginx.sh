2d5f7279d7aafcd915821d4dd58afd5df6253be6
枫雨 <737345039@qq.com>
2022-03-06 21:26:13 +0800

    chore: Update
    Update README.md
    
    Update .sh file
    
    Update default.conf.template
    
    Update prepare.sh
    
    Update DDTV_WEB_Server_Docker.yml

--- nginx/docker-entrypoint.d/nginx.sh
+++ nginx/docker-entrypoint.d/nginx.sh
@@ -1,43 +1,60 @@
 #!/bin/sh
 #
-HTMLPATH=/usr/share/nginx/html
+echo '
+    ____  ____  _______    __     _____  ____
+   / __ \/ __ \/_  __/ |  / /    |__  / / __ \
+  / / / / / / / / /  | | / /      /_ < / / / /
+ / /_/ / /_/ / / /   | |/ /     ___/ // /_/ /
+/_____/_____/ /_/    |___/     /____(_)____/
 
-FILEPATH=$HTMLPATH/static/config.js
-if [ -n "$apiUrl" ]; then
-    sed -i "/apiUrl/s|:.*,|: \"$apiUrl\",|" $FILEPATH
-fi
-if [ -n "$mount" ]; then
-    sed -i "/mount/s|'.*'|'$mount'|" $FILEPATH
+     _       ____________     __  ______
+    | |     / / ____/ __ )   / / / /  _/
+    | | /| / / __/ / __  |  / / / // /
+    | |/ |/ / /___/ /_/ /  / /_/ // /
+    |__/|__/_____/_____/   \____/___/
+'
+
+# 检测 DDTV 目录文件是否齐全
+Backups_Path=/DDTV_Backups
+if [ "$(ls -A $Backups_Path)" ]; then
+    shopt -s globstar nullglob
+    for file in test/**; do
+        [ "${file##"$Backups_Path"/}" = "" ] && continue
+        [ ! -e "${file##"$Backups_Path"/}" ] && cp -vur "$file" "${file##"$Backups_Path"/}"
+    done
 fi
 
-FILEPATH=$HTMLPATH/static/barinfo.js
-if [ -n "$show" ]; then
-    sed -i "/    show/s|: .*,|: $show,|" $FILEPATH
-fi
-if [ -n "$infoshow" ]; then
-    sed -i "/info.*show/s|show:.*text|show: $infotext, text|" $FILEPATH
-fi
-if [ -n "$infotext" ]; then
-    sed -i "/info.*text/s|text.*link|text: \"$infotext\", link|" $FILEPATH
-fi
-if [ -n "$infolink" ]; then
-    sed -i "/info.*link/s|link.*}|link: \"$infolink\" }|" $FILEPATH
-fi
-if [ -n "$ICPshow" ]; then
-    sed -i "/ICP.*show/s|show.*text|show: $ICPshow, text|" $FILEPATH
-fi
-if [ -n "$ICPtext" ]; then
-    sed -i "/ICP.*show/s|text.*link|show: \"$ICPtext\", link|" $FILEPATH
-fi
-if [ -n "$ICPlink" ]; then
-    sed -i "/ICP.*link/s|link.*}|link: \"$ICPlink\" }|" $FILEPATH
-fi
-if [ -n "$GAshow" ]; then
-    sed -i "/GA.*show/s|show.*text|show: $GAshow, text|" $FILEPATH
-fi
-if [ -n "$GAtext" ]; then
-    sed -i "/GA.*show/s|text.*link|show: \"$GAtext\", link|" $FILEPATH
-fi
-if [ -n "$GAlink" ]; then
-    sed -i "/GA.*link/s|link.*}|link: \"$GAlink\" }|" $FILEPATH
+# 第一次启动修改信息
+if [ ! -e "/NotIsFirstStart" ]; then
+    HTMLPATH=/usr/share/nginx/html
+
+    FILEPATH=$HTMLPATH/static/config.js
+    if [ -n "$apiUrl" ]; then
+        sed -i "/apiUrl/s|:.*,|: \"$apiUrl\",|" $FILEPATH ; fi
+    if [ -n "$mount" ]; then
+        sed -i "/mount/s|'.*'|'$mount'|" $FILEPATH ; fi
+
+    FILEPATH=$HTMLPATH/static/barinfo.js
+    if [ -n "$show" ]; then
+        sed -i "/    show/s|: .*,|: $show,|" $FILEPATH ; fi
+    if [ -n "$infoshow" ]; then
+        sed -i "/info.*show/s|show:.*text|show: $infotext, text|" $FILEPATH ; fi
+    if [ -n "$infotext" ]; then
+        sed -i "/info.*text/s|text.*link|text: \"$infotext\", link|" $FILEPATH ; fi
+    if [ -n "$infolink" ]; then
+        sed -i "/info.*link/s|link.*}|link: \"$infolink\" }|" $FILEPATH ; fi
+    if [ -n "$ICPshow" ]; then
+        sed -i "/ICP.*show/s|show.*text|show: $ICPshow, text|" $FILEPATH ; fi
+    if [ -n "$ICPtext" ]; then
+        sed -i "/ICP.*show/s|text.*link|show: \"$ICPtext\", link|" $FILEPATH ; fi
+    if [ -n "$ICPlink" ]; then
+        sed -i "/ICP.*link/s|link.*}|link: \"$ICPlink\" }|" $FILEPATH ; fi
+    if [ -n "$GAshow" ]; then
+        sed -i "/GA.*show/s|show.*text|show: $GAshow, text|" $FILEPATH ; fi
+    if [ -n "$GAtext" ]; then
+        sed -i "/GA.*show/s|text.*link|show: \"$GAtext\", link|" $FILEPATH ; fi
+    if [ -n "$GAlink" ]; then
+        sed -i "/GA.*link/s|link.*}|link: \"$GAlink\" }|" $FILEPATH ; fi
+
+    touch /NotIsFirstStart
 fi
