606964888466a37c6010619760865cc16374e694
枫雨 <737345039@qq.com>
2022-03-06 21:37:42 +0800

    Update prepare.sh

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e; set -u
 wget -q "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - https://api.github.com/repos/CHKZL/DDTV/releases/latest | awk '/[Ss]erver/{print $4;exit}' FS='"')"
-Server_File_Path=$(unzip -l ./*.zip | awk "/files:.*\.zip/{print \$2;exit}" FS='/')
+Server_File_Path=$(unzip -l ./*.zip | awk "/dll/{print \$4;exit}" FS=' ')
 
-#wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk '/[Ss]erver/{print $4;exit}' FS='"')"
+#wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk "/files:.*\.zip/{print \$2;exit}" FS='/')"
 wget "$1"
 unzip "*.zip"
 
