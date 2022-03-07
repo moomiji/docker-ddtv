04b5829e684c20e2346abfcf48ccb615fb10f627
枫雨 <737345039@qq.com>
2022-03-08 03:01:28 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -4,13 +4,13 @@ wget -q "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - htt
 Server_File_Path=$(unzip -l ./*.zip | awk "/dll/{print \$4;exit}" FS=' ')
 
 #wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk "/files:.*\.zip/{print \$2;exit}" FS='/')"
-wget "$1"
+wget -q "$1"
 unzip "*.zip"
 
 mkdir -p root/DDTV \
          root/DDTV_Backups \
          nginx/DDTV \
-         nginx/DDTV_Backups
+         nginx/DDTV_Backups/static
 mv -v dist/* nginx/DDTV_Backups/static
 mv -v "${Server_File_Path%/*}"/* root/DDTV_Backups
 cp -v nginx/docker-entrypoint.d/webui.sh root/webui.sh
