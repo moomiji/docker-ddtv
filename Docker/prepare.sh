76c0da8354277920bc8e698318022241b0ec5468
枫雨 <737345039@qq.com>
2022-03-20 19:12:47 +0800

    Update to DDTV Ver3.0.2.1

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -4,15 +4,18 @@ wget -q "https://github.com/CHKZL/DDTV/releases/latest/download/$(wget -qO - htt
 Server_File_Path=$(unzip -l ./*.zip | awk "/dll/{print \$4;exit}" FS=' ')
 
 #wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk "/files:.*\.zip/{print \$2;exit}" FS='/')"
-wget -q https://github.com/moomiji/docker-ddtv/releases/download/edge/webui0.0.1a.zip
+wget -q https://github.com/moomiji/docker-ddtv/releases/download/edge/static.zip
 unzip "*.zip"
 
 mkdir -p root/DDTV \
          root/DDTV_Backups \
          nginx/DDTV \
          nginx/DDTV_Backups/static
-mv -v dist/* nginx/DDTV_Backups/static
-mv -v "${Server_File_Path%/*}"/* root/DDTV_Backups
-cp -v nginx/docker-entrypoint.d/webui.sh root/webui.sh
+mv -v dist/* \
+         nginx/DDTV_Backups/static
+mv -v "${Server_File_Path%/*}"/* \
+         root/DDTV_Backups
+cp -v nginx/docker-entrypoint.d/webui.sh \
+         root/webui.sh
 
 # cd root/DDTV_Backups && dotnet DDTV_Update.dll docker
