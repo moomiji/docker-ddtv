71cd41ad5223a606a4555c46b64413fa3026ad8d
枫雨 <737345039@qq.com>
2022-04-08 07:01:16 +0800

    fix filename not matched

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -10,12 +10,15 @@ done
 
 # 下载 DDTV
 
-wget -q https://api.github.com/repos/CHKZL/DDTV/releases/latest
-wget -q -O CLI.zip "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i CLI)"
-wget -q -O WEBServer.zip "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i Server)"
+wget -q https://api.github.com/repos/CHKZL/DDTV/releases/latest &&
+    echo "latest downloaded"
+wget -q -O CLI.zip "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i CLI)" &&
+    echo "CLI.zip downloaded"
+wget -q -O WEBServer.zip "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i Server)" &&
+    echo "WEBServer.zip downloaded"
 CLI_File_Path=$(   unzip -l CLI.zip       | awk "/dll/{print \$4;exit}" FS=' ')
 Server_File_Path=$(unzip -l WEBserver.zip | awk "/dll/{print \$4;exit}" FS=' ')
-unzip CLI.zip WEBserver.zip
+unzip "*.zip"
 
 # wget -q "https://github.com/hegugu-ng/DDTV_WEBUI/releases/latest/download/$(wget -qO - https://raw.githubusercontent.com/hegugu-ng/DDTV_WEBUI/main/.github/workflows/npm-publish-github-packages.yml | awk "/files:.*\.zip/{print \$2;exit}" FS='/')"
 # wget -q https://github.com/moomiji/docker-ddtv/releases/download/edge/static.zip
@@ -38,12 +41,12 @@ mv -v "${CLI_File_Path%/*}/*" \
 mv -v "${Server_File_Path%/*}/*" \
          WEBServer/root/DDTV_Backups
 
-echo CLI/            \
-     Debug/          \
-     WEBUI/          \
-     WEBServer/      | xargs -n 1 cp -v ./install.sh
+echo CLI            \
+     Debug          \
+     WEBUI          \
+     WEBServer      | xargs -n 1 cp -v ./install.sh
 
-echo CLI/root/       \
-     Debug/root/     \
-     WEBUI/root/     \
-     WEBServer/root/ | xargs -n 1 cp -v ./checkup.sh
+echo CLI/root       \
+     Debug/root     \
+     WEBUI/root     \
+     WEBServer/root | xargs -n 1 cp -v ./checkup.sh
