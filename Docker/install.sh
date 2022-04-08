fdfad0dbcda8d0116f237ba760198624c17ddb11
枫雨 <737345039@qq.com>
2022-04-08 08:49:13 +0800

    fix

--- Docker/install.sh
+++ Docker/install.sh
@@ -17,7 +17,7 @@ case $ID in
         ;;
 
     debian)
-        apt update
+        # apt update
         if [[ "$DDTV_Project" == "Debug" ]]; then
             apt install --no-install-recommends ffmpeg bash tzdata wget unzip gosu libgdiplus perf perl git -y
             git clone --depth=1 https://github.com/BrendanGregg/FlameGraph
