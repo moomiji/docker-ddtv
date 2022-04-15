da5e26031348a80209cdbc96d1ddef067bee94cf
枫雨 <737345039@qq.com>
2022-04-15 22:52:38 +0800

    chore: Fix
    Update Docker_Build_Push.yml
    
    Update DDTV_Docker_Release.yml
    
    fix /runs/6040480093
    
    fix /runs/6040019797
    
    Fix /runs/6039759590
    
    Fix
    
    Fix

--- Docker/prepare.sh
+++ Docker/prepare.sh
@@ -6,40 +6,40 @@ set -e; set -u
 # 复用 Dockerfile
 sed -i "/FROM/s/$/&${2:-}/g" "$1/Dockerfile"
 case $1 in
-    *deps)
+    ddtv/deps)
         exit 0
         ;;
-    *cli)
+    ddtv/cli)
         Keyword=CLI
         ;;
-    *webserver)
-        Keyword=WEBServer
+    ddtv/webserver)
+        Keyword=Server
         ;;
-    *webui)
-        Keyword=WEBServer
+    ddtv/webui)
+        Keyword=Server
         is_nginx=true
         ;;
 esac
         KeyFile=DDTV_Core.dll
 
 # 下载DDTV
-wget -q https://api.github.com/repos/CHKZL/DDTV/releases/latest                                      \
-     && wget -q  "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword )"       \
-     && File_Path=$(cat < latest | awk '/name/{print $4}' FS='"'         | grep -i $Keyword )        \
-     && echo "$File_Path downloaded"                                                                 \
-     && File_Path=$(unzip $File_Path | awk "/$KeyFile/{print \$2}" | awk '{print $1}' FS="$KeyFile") \
-     && echo "File path geted"
+wget --no-verbose https://api.github.com/repos/CHKZL/DDTV/releases/latest                               \
+     && wget --no-verbose "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword )" \
+     && File_Path=$(         cat < latest | awk '/name/{print $4}' FS='"'         | grep -i $Keyword )  \
+     && File_Path=$(unzip "$File_Path" | awk "/$KeyFile/{print \$2}" | awk '{print $1}' FS="/$KeyFile") \
+     && echo "File path geted"                                                                          \
+     || eval 'echo "Failed to get File path" && exit 1'
 
 # 转移DDTV
-mkdir -vp "$1/root/DDTV"         \
-          "$1/root/DDTV_Backups"
-mv -v "$File_Path/*"             \
+mkdir -vp "$1/root/DDTV"
+mv -v "$File_Path"               \
           "$1/root/DDTV_Backups"
 mv -v ./checkup.sh               \
           "$1/root"
 
+shopt -s extglob
 if [ -n "${is_nginx:-}" ]; then
-    mv -v "$1/root/checkup.sh"  \
+    mv -v "$1/root/checkup.sh"   \
           "$1/root/docker-entrypoint.d/01-checkup.sh"
     cd "$1/root/DDTV_Backups"
     rm -rf !(keep|keep2)
