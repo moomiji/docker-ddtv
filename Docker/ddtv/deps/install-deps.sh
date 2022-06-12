20f062e4d8a04651761d4041ca5967a166e36f73
枫雨 <737345039@qq.com>
2022-06-13 00:16:34 +0800

    remove libgdiplus

--- Docker/ddtv/deps/install-deps.sh
+++ Docker/ddtv/deps/install-deps.sh
@@ -7,13 +7,13 @@ set -e; set -u
 case $ID in
     alpine)
         apk add --no-cache ffmpeg bash tzdata su-exec
-        apk add libgdiplus --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+        # apk add libgdiplus --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
         sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
         ;;
 
     debian|ubuntu)
         apt update
-        apt install --no-install-recommends ffmpeg bash tzdata gosu libgdiplus -y
+        apt install --no-install-recommends ffmpeg bash tzdata gosu -y # libgdiplus
         sed -i 's/deb.debian.org/mirrors.aliyun.com/g'     /etc/apt/sources.list
         sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
         ;;
