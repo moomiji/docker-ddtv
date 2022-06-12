7bd79205100c67b311204a73fea06b85fdc6143f
枫雨 <737345039@qq.com>
2022-06-13 01:12:59 +0800

    Use 7z instead of unzip
    Extract .7z .rar .zip

--- Docker/prepare.GitHubActions.sh
+++ Docker/prepare.GitHubActions.sh
@@ -16,15 +16,17 @@ case $1 in
         is_nginx=true
         ;;
 esac
-        KeyFile=DDTV_Core.dll
+KeyFile=DDTV_Core.dll
 
 # 下载DDTV
 wget --no-verbose https://api.github.com/repos/CHKZL/DDTV/releases/latest                               \
      && wget --no-verbose "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword )" \
      && File_Path=$(         cat < latest | awk '/name/{print $4}' FS='"'         | grep -i $Keyword )  \
-     && File_Path=$(unzip "$File_Path" | awk "/$KeyFile/{print \$2}" | awk '{print $1}' FS="/$KeyFile") \
-     && echo "File path geted"                                                                          \
-     || eval 'echo "Failed to get File path" && exit 1'
+     && 7z x -bd "$File_Path"                                                                           \
+     && echo "Extract ($File_Path) succeeded"                                                           \
+     && File_Path=$(7z l "$File_Path" | awk "/$KeyFile/{print \$6}" | awk '{print $1}' FS="/$KeyFile")  \
+     && echo "File path ($File_Path) geted"                                                             \
+     || eval 'echo "Failed to get File path OR extract" && exit 1'
 
 # 转移DDTV
 mkdir -vp "$1/root/DDTV"
