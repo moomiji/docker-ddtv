303192302a0992de21abdac97ffaa58d45120bf3
枫雨 <737345039@qq.com>
2022-11-04 23:06:14 +0800

    Update prepare.GitHubActions.sh

--- Docker/prepare.GitHubActions.sh
+++ Docker/prepare.GitHubActions.sh
@@ -22,7 +22,7 @@ KeyFile=DDTV_Core.dll
 wget --no-verbose https://github.com/moomiji/docker-ddtv/releases/download/edge/webui-20221104.zip
 7z x -bd webui-20221104.zip
 ls -al
-mv -v disk/ static/
+mv -v ./disk ./static
 File_Path=./static
 
 
