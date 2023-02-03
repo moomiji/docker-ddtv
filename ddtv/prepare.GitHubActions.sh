331850673ace1ee1b1edf5c3b6a1f0c02fcdb772
枫雨 <737345039@qq.com>
2023-02-03 23:06:08 +0800

    chore: 准备主仓库子模块

--- ddtv/prepare.GitHubActions.sh
+++ ddtv/prepare.GitHubActions.sh
@@ -13,10 +13,12 @@ case $1 in
         ;;
     ddtv/webui)
         Keyword=Server
-        is_nginx=true
+        is_webui=true
         ;;
 esac
 KeyFile=DDTV_Core.dll
+shopt -s globstar nullglob
+cd ./**/$1
 
 # 下载DDTV
 wget --no-verbose https://api.github.com/repos/CHKZL/DDTV/releases/latest                               \
@@ -29,17 +31,17 @@ wget --no-verbose https://api.github.com/repos/CHKZL/DDTV/releases/latest       
      || eval 'echo "Failed to get File path OR extract" && exit 1'
 
 # 转移DDTV
-mkdir -vp "$1/root/DDTV"
-mv -v "$File_Path"               \
-          "$1/root/DDTV_Backups"
-mv -v ./*-checkup.sh             \
-          "$1/root/docker-entrypoint.d"
-mv -v ./docker-entrypoint.sh     \
-          "$1/root"
+mkdir -vp ./root/DDTV
+mv -v $File_Path                \
+          ./root/DDTV_Backups
+mv -v ../docker-entrypoint.sh \
+          ./root
+mv -v ../*-*.sh         \
+          ./root/docker-entrypoint.d
 
 shopt -s extglob
-if [ -n "${is_nginx:-}" ]; then
-    rm  "$1/root/docker-entrypoint.sh"
-    cd  "$1/root/DDTV_Backups"
+if [ -n "${is_webui:-}" ]; then
+    rm  "./root/docker-entrypoint.sh"
+    cd  "./root/DDTV_Backups"
     rm -rf !(keep|keep2)
 fi
