636ca6f59eb82b75e46ecf674724ab606880846a
枫雨 <737345039@qq.com>
2023-02-04 10:49:50 +0800

    Squashed 'Docker/' content from commit 3318506
    git-subtree-dir: Docker
    git-subtree-split: 331850673ace1ee1b1edf5c3b6a1f0c02fcdb772

---
+++ ddtv/prepare.GitHubActions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e; set -u
+# $1: $*_REPO in Docker_Release.yml#L28-40
+case $1 in
+    ddtv/deps)
+        exit 0
+        ;;
+    ddtv/cli)
+        Keyword=CLI
+        ;;
+    ddtv/webserver)
+        Keyword=Server
+        ;;
+    ddtv/webui)
+        Keyword=Server
+        is_webui=true
+        ;;
+esac
+KeyFile=DDTV_Core.dll
+shopt -s globstar nullglob
+cd ./**/$1
+
+# 下载DDTV
+wget --no-verbose https://api.github.com/repos/CHKZL/DDTV/releases/latest                               \
+     && wget --no-verbose "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword )" \
+     && File_Path=$(         cat < latest | awk '/name/{print $4}' FS='"'         | grep -i $Keyword )  \
+     && 7z x -bd "$File_Path"                                                                           \
+     && echo "Extract ($File_Path) succeeded"                                                           \
+     && File_Path=$(7z l "$File_Path" | awk "/$KeyFile/{print \$6}" | awk '{print $1}' FS="/$KeyFile")  \
+     && echo "File path ($File_Path) geted"                                                             \
+     || eval 'echo "Failed to get File path OR extract" && exit 1'
+
+# 转移DDTV
+mkdir -vp ./root/DDTV
+mv -v $File_Path                \
+          ./root/DDTV_Backups
+mv -v ../docker-entrypoint.sh \
+          ./root
+mv -v ../*-*.sh         \
+          ./root/docker-entrypoint.d
+
+shopt -s extglob
+if [ -n "${is_webui:-}" ]; then
+    rm  "./root/docker-entrypoint.sh"
+    cd  "./root/DDTV_Backups"
+    rm -rf !(keep|keep2)
+fi
