06e29495efc57ccc4c2f1ac06743b39d0f57c393
枫雨 <35213527+moomiji@users.noreply.github.com>
2024-02-24 17:47:48 +0800

    feat: 在 path 中寻找 DDTV 主程序路径

--- ddtv/prepare.GitHubActions.sh
+++ ddtv/prepare.GitHubActions.sh
@@ -14,15 +14,18 @@ cd ./**/"$1"
 
 header="authorization: Bearer $2"
 # 下载DDTV
-if [[ -n "${3:-}" ]]; then File_Path=$3; else
-curl -sfLOH "$header" "https://api.github.com/repos/CHKZL/DDTV/releases/latest"                             \
-     && curl -sfLOH "$header" "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword -m 1 )" \
-     && File_Path="$(            cat < latest | awk '/name/{print $4}'         FS='"' | grep -i $Keyword -m 1 )" \
-     && 7z x -bd "$File_Path"                                                                               \
-     && echo "Extract ($File_Path) succeeded"                                                               \
-     && File_Path=$(7z l "$File_Path" | awk "/$KeyFile/{print \$6}" | awk '{print $1}' FS="/$KeyFile")      \
-     && echo "File path ($File_Path) geted"                                                                 \
-     || eval 'echo "Failed to get File path OR extract" && exit 1'
+if [[ -n "${3:-}" ]]; then
+    File_Path=$3
+    File_Path=$(find "$File_Path" -name "$KeyFile" | awk '{print $1}' FS="/$KeyFile")
+else
+    curl -sfLOH "$header" "https://api.github.com/repos/CHKZL/DDTV/releases/latest"                             \
+        && curl -sfLOH "$header" "$(cat < latest | awk '/download_url/{print $4}' FS='"' | grep -i $Keyword -m 1 )" \
+        && File_Path="$(            cat < latest | awk '/name/{print $4}'         FS='"' | grep -i $Keyword -m 1 )" \
+        && 7z x -bd "$File_Path"                                                                               \
+        && echo "Extract ($File_Path) succeeded"                                                               \
+        && File_Path=$(7z l "$File_Path" | awk "/$KeyFile/{print \$6}" | awk '{print $1}' FS="/$KeyFile")      \
+        && echo "File path ($File_Path) geted"                                                                 \
+        || eval 'echo "Failed to get File path OR extract" && exit 1'
 fi
 
 # 转移DDTV
